43c3aecbd3838072c6422d0d611d9b958dcf1802
Vash Salarda <vashful.v2@gmail.com>
2023-01-06 15:34:43 +0800

    Update migrations

---
+++ src/Services/Employee/Employee.API/Migrations/20230106072841_InitialMigration.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Employee.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    userid = table.Column<string>(type: "text", nullable: false),
+                    contactnumber = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    branch = table.Column<string>(type: "text", nullable: false),
+                    city = table.Column<string>(type: "text", nullable: true),
+                    state = table.Column<string>(type: "text", nullable: true),
+                    region = table.Column<string>(type: "text", nullable: true),
+                    title = table.Column<string>(type: "text", nullable: true),
+                    department = table.Column<string>(type: "text", nullable: true),
+                    position = table.Column<string>(type: "text", nullable: true),
+                    designation = table.Column<string>(type: "text", nullable: true),
+                    datehired = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    sss = table.Column<string>(type: "text", nullable: true),
+                    phic = table.Column<string>(type: "text", nullable: true),
+                    pagibig = table.Column<string>(type: "text", nullable: true),
+                    tin = table.Column<string>(type: "text", nullable: true),
+                    bankname = table.Column<string>(type: "text", nullable: true),
+                    accountname = table.Column<string>(type: "text", nullable: true),
+                    accountnumber = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    firstname = table.Column<string>(type: "text", nullable: false),
+                    lastname = table.Column<string>(type: "text", nullable: false),
+                    mobilenumber = table.Column<string>(type: "text", nullable: false),
+                    country = table.Column<string>(type: "text", nullable: false),
+                    state = table.Column<string>(type: "text", nullable: false),
+                    city = table.Column<string>(type: "text", nullable: false),
+                    role = table.Column<string[]>(type: "text[]", nullable: false),
+                    designation = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employees_id",
+                table: "employees",
+                column: "id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
